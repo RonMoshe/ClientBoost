1085ae573fb902393bf86cc2f1dac0702410c12b
Adi-Tamir <tamiri@post.bgu.ac.il>
2021-01-08 19:30:37 +0200

    supports commands that use short!

--- src/EncodeDecode.cpp
+++ src/EncodeDecode.cpp
@@ -52,54 +52,43 @@ std::string EncodeDecode:: Encode(std::string line){
 
     if(opcode.compare("ADMINREG") == 0){
         op = 1;
-        //enc = line.substr(currIndex+1);
     }
     else if(opcode.compare("STUDENTREG") == 0){
         op = 2;
-        //enc = line.substr(currIndex+1);
     }
     else if(opcode.compare("LOGIN") == 0){
         op = 3;
-        //enc = line.substr(currIndex+1);
     }
     else if(opcode.compare("LOGOUT") == 0){
         op = 4;
     }
     else if(opcode.compare("COURSEREG") == 0){
         op = 5;
-        //enc = line.substr(currIndex+1);
     }
     else if(opcode.compare("KDAMCHECK") == 0){
         op = 6;
-        //enc = line.substr(currIndex+1);
     }
     else if(opcode.compare("COURSESTAT") == 0){
         op = 7;
-        //enc = line.substr(currIndex+1);
     }
     else if(opcode.compare("STUDENTSTAT") == 0){
         op = 8;
-        //enc = line.substr(currIndex+1);
     }
     else if(opcode.compare("ISREGISTERED") == 0){
         op = 9;
-        //enc = line.substr(currIndex+1);
     }
     else if(opcode.compare("UNREGISTER") == 0){
         op = 10;
-        //enc = line.substr(currIndex+1);
     }
     else if(opcode.compare("MYCOURSES") == 0){
         op = 11;
     }
 
-
     char* opc = new char[2];
     shortToBytes(op, opc);
 
     std::string msg = "";
-    //msg.push_back(opc[0]);
-    //msg.push_back(opc[1]);
+
     msg.append(1, opc[0]);
     msg.append(1, opc[1]);
     int count;
@@ -121,17 +110,13 @@ std::string EncodeDecode:: Encode(std::string line){
         }
         else {
             msg = msg + s;
-
         }
         msg.append(1, '\0');
         enc = enc.substr(index + 1);
-
     }
-
     if(op == 4 || op == 8) {
         msg.append(1, '\0');
     }
-    //msg.append(1, '\0');
     std::cout<<msg<<std::endl;
     std::cout<<msg + " GRRR"<<std::endl;
     return msg;
